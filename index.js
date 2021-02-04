5f3e5019335b3492ff6ee278cceb8219d739a855
medicoconsultation <78396395+medicoconsultation@users.noreply.github.com>
2021-02-04 16:05:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const http=require('http')
-const app=require('./app.js')
+const http = require('http');
+const app=require('./app');
+
+const hostname='0.0.0.0';
+const port =3000;
 
 
-const port=3000
+const server = http.createServer(app);
 
 
-server=http.createServer(app)
-
-
-
-server.listen(port);
+server.listen(port,hostname,()=>{
+    console.log('server running at http://'+hostname+':'+port+'/');
+});
